8d8e48731feea92f635dbd72f307b0b11d4e357a
12yuens2 <sizhe1007@gmail.com>
2018-01-28 13:24:47 +0000

    Remove old files.

--- copy.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-subprocess.run(["cp", "/tmp/sy35/exps/result/sched_stats.txt", "."])
--- script.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-subprocess.run(["build/ARM/gem5.opt", "-d", "/tmp/sy35/exps/result", "configs/sched-bodytrack_gen.py", "--machine-type=VExpress_EMM64"])
